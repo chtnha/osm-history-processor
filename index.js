8be8bd32fa837740103fe62388e9a403ab43afdc
Rub21 <ruben@mapbox.com>
2015-11-25 00:43:36 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
 var crontab = require('node-crontab');
-crontab.scheduleJob("10 00 * * *", function() {
+crontab.scheduleJob("*/2 * * * *", function() {
+	console.log("running");
 	function puts(error, stdout, stderr) {
 		sys.puts(stdout)
 	}
-	exec("~/home/penny/osm-history-processor/buildosm.sh", puts);
+	exec("/home/penny/osm-history-processor/buildosm.sh", puts);
 });
